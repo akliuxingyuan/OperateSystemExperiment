7c415bb51a325f607478fdb2c123c1c80f49ba31
akliuxingyuan <akliuxingyuan@gmail.com>
2021-04-18 21:42:01 +0800

    Add Experiment1

---
+++ MobileAppsDev/Experiment1/myvue01/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <div>
+    <button @click="visible = !visible">{{ visible===true?'隐藏':'显示'}}</button>
+    <HelloWorld v-show="visible" msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  data() {
+    return {
+      visible:false
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ MobileAppsDev/Experiment1/myvue02/src/components/Login.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="login-box">
+    <p class="username">用户名：<input type="text" v-model="userName" /></p>
+    <p class="userpass">密码：<input type="text" v-model="passWord" /></p>
+    <button @click="login">登录</button>
+    <div v-show="infovisible">
+      <p>{{ msg }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+// import axios from 'axios'
+
+export default {
+  name: "login",
+  data() {
+    return {
+      userName: "",
+      passWord: "",
+      infovisible: false,
+      msg: "用户名密码错误",
+    };
+  },
+  methods: {
+    login() {
+      if (this.userName === "root" && this.passWord === "root") {
+        console.log("登录成功");
+        // 页面进行跳转
+        this.msg = "登录成功";
+      }
+      this.infovisible = true;
+    },
+  },
+};
+</script>
+
+<style scoped>
+.login-box {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.username {
+  display: flex;
+  flex-direction: row;
+  justify-content: right;
+  width: 300px;
+}
+
+.userpass {
+  display: flex;
+  flex-direction: row;
+  justify-content: right;
+  width: 300px;
+}
+</style>
